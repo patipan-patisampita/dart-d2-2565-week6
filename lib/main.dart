d12a4276633edfae0c7385af9bdc1d30b476af44
patipan-patisampita <patipan.patisampita@gmail.com>
2022-06-23 15:55:21 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'app.dart';
+import 'hello.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Flutter App",
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Flutter App"),
-        ),
-        body: Center(
-          child: Text(
-            "Count:",
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.deepOrange,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            debugPrint("count:");
-          },
-          child: Icon(Icons.add_alarm_sharp),
-        ),
-      ),
-    );
-  }
-}
+
+
+
+
