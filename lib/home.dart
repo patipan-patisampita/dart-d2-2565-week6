4242215c2aad0d6fa59297af882dde0cae9b9aa2
patipan-patisampita <patipan.patisampita@gmail.com>
2022-06-23 16:12:39 +0700

    update

--- lib/home.dart
+++ lib/home.dart
@@ -1,30 +1,28 @@
 import 'package:flutter/material.dart';
 
-class HelloWorld extends StatelessWidget {
-  const HelloWorld({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text("Flutter App"),
+        title: Text("Flutter Basic"),
       ),
       body: Center(
         child: Text(
-          "Hello World",
+          "Count:",
           style: TextStyle(
-            fontSize: 30,
+            fontSize: 40,
             color: Colors.deepOrange,
             fontWeight: FontWeight.bold,
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          debugPrint("count:");
-        },
-        child: Icon(Icons.add_alarm_sharp),
+        onPressed: () {},
+        child: Icon(Icons.add_alarm),
       ),
     );
   }
